81e1d74a2258da2395ae983452cf122cc3130607
wangzhe <wangzhe0912@tju.edu.cn>
2017-01-23 21:14:51 +0800

    log in

--- model.py
+++ model.py
@@ -7,4 +7,16 @@ Created on Sat Jan 21 21:46:48 2017
 
 import web
 
-db = web.database(dbn='mysql', db='blog', user='root', pw='Qxq!102132', charset='utf8')
+db = web.database(dbn='mysql', db='nianshi', user='root', pw='Qxq!102132', charset='utf8')
+
+def check_pwd(username, password):
+    records = db.select('login', where='username=$username', vars=locals())
+    if len(records) == 0:
+        result = {"status": False, "result": None, "desc": "username doesn't exist!"}
+    else:
+        record = records[0]
+        if record.password == password:
+            result = {"status": True, "result": record, "desc": "Log in successful!"}
+        else:
+            result = {"status": False, "result": None, "desc": "password is not correct!"}
+    return result
--- nianshi.py
+++ nianshi.py
@@ -9,6 +9,7 @@ import web
 from urlparse import unquote
 import sys
 import os
+import model
 
 os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
@@ -28,12 +29,48 @@ urls = (
         '/blog', 'Blog',
         '/tools', 'Tools',
         '/video', 'Video',
+        '/logout', 'Logout',
+        '/log_out', 'LogOut',
         '/', 'Hello',)
 
 class Login(object):
-    def GET(self):
-        return u"请登录"
+    def check_pwd(self, username, password):
+        result = model.check_pwd(username, password)
+        return result
+        
 
+    def GET(self):
+        return render.login()
+
+    def POST(self):
+        data = web.input()
+        username = data['form-username']
+        password = data['form-password']
+        result = self.check_pwd(username, password)
+        if not result["status"]:
+            return render.homepage(False, result["desc"])
+        else:
+            web.setcookie('name', result['result']['username'], 3600)
+            web.setcookie('admin', result['result']['admin'], 3600)
+            web.setcookie('shizi', result['result']['shizi'], 3600)
+            return render.homepage(True, result['result']['username'])
+        
+
+class Logout(object):
+    def GET(self):
+        web.cookies().clear()
+        print web.cookies()
+        #web.redirect('/')
+        return 1+1
+
+
+class LogOut(object):
+    def GET(self):
+        web.cookies()
+        print web.cookies()
+        #web.redirect('/')
+        return 1+1
+        
 
 class About(object):
     def GET(self):
@@ -62,7 +99,10 @@ class Video(object):
 
 class Hello(object):
     def GET(self):
-        return render.homepage()
+        if 'name' in web.cookies():
+            return render.homepage(True, web.cookies().name)
+        else:
+            return render.homepage(False, '')
 
 
 class Index(object):
